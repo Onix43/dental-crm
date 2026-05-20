6409a80fe7ad535c5a58ab13dd4a3e2798160835
Onix <piyvoda20033@gmail.com>
2026-05-20 13:26:06 +0300

    completed initial structure

---
+++ server/src/app.ts
@@ -0,0 +1,17 @@
+import express from "express";
+import cors from "cors";
+import { pinoLogger } from "./middlewares/pinoLogger.js";
+
+export const app = express();
+
+// Middlewares
+app.use(pinoLogger);
+app.use(express.json());
+app.use(cors());
+
+//Routes
+
+//Test route
+app.get("/api/health", (req, res) => {
+  res.status(200).json({ success: "true" });
+});
